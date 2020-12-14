189bd145312fe77dcdd0c6142a466b3cd5ef745d
“austin_hung” <austin_h_hung@trendmicro.com>
2020-12-14 21:48:52 +0800

    fix: type validation util

--- src/utils/type-validation.ts
+++ src/utils/type-validation.ts
@@ -4,12 +4,13 @@
  * Check the type of the input string, for exmaple: Email, IP, URL, etc.
  * --------------------------------------------------------------------------
  */
-
-const EMAIL_REGEXP = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+const REGEX = {
+  'email': /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+}
 
 export default function validateType(type: string, str: string) {
   if (type === 'email') {
-    return EMAIL_REGEXP.test(str);
+    return REGEX['email'].test(str);
   }
   return false;
 }
