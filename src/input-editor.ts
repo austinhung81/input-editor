eeb37d1a543ae53e6414accdfb651fb8e93dcc33
austin_hung <austin_h_hung@trendmicro.com>
2020-12-15 16:47:21 +0800

    fix: typescript type and code refine

--- src/input-editor.ts
+++ src/input-editor.ts
@@ -12,6 +12,8 @@ const NAME = 'input-editor';
 
 const VALID = 'valid';
 const INVALID = 'invalid';
+
+const INPUTTYPE = 'email';
 const PLACEHOLDER = 'Enter value...';
 
 interface InputEditorProps {
@@ -36,7 +38,7 @@ class InputEditor {
   constructor(element: HTMLElement, config: InputEditorProps = {}) {
     const {
       source = [],
-      inputType = 'email',
+      inputType = INPUTTYPE,
       placeholder = PLACEHOLDER,
     } = config;
     this._inputType = inputType;
@@ -56,7 +58,7 @@ class InputEditor {
     this._container.appendChild(this._input);
     this._container.addEventListener('click', this._handleInputFocus);
 
-    this._setTokens(source);
+    !!source.length && this._checkIsToken(source);
 
     element.appendChild(this._container);
   }
@@ -73,27 +75,27 @@ class InputEditor {
 
   // Public
 
-  createToken = (tokens: string[] | string) => {
-    this._setTokens(tokens);
+  createToken = (tokens: string[] | string): void => {
+    this._checkIsToken(tokens);
   };
 
   // Private
 
   _createTokenElement = (token: string): HTMLElement => {
-    const isValid = validateType(this._inputType, token);
-    const className = `${NAME}-token ${isValid ? VALID : INVALID}`;
-    const tokenElement = document.createElement('span');
+    const isValid: boolean = validateType(this._inputType, token);
+    const className: string = `${NAME}-token ${isValid ? VALID : INVALID}`;
+    const tokenElement: HTMLElement = document.createElement('span');
     tokenElement.className = className;
     tokenElement.innerText = token;
-    const removeBtn = this._createRemoveButton(tokenElement);
+    const removeBtn: HTMLElement = this._createRemoveButton(tokenElement);
     tokenElement.appendChild(removeBtn);
 
     return tokenElement;
   };
 
   _createRemoveButton = (elm: HTMLElement): HTMLElement => {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    const removeBtn = document.createElement('span');
+    const svg: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const removeBtn: HTMLElement = document.createElement('span');
     svg.setAttribute('viewBox', '0 0 24 24')
     svg.setAttribute('fill', 'none')
     svg.innerHTML = `<path fill-rule="evenodd" clip-rule="evenodd" d="M10.5858 12L4.2218 5.63602L5.63602 4.2218L12 10.5858L18.3639 4.2218L19.7782 5.63602L13.4142 12L19.7782 18.3639L18.3639 19.7782L12 13.4142L5.63602 19.7782L4.2218 18.3639L10.5858 12Z" />`;
@@ -106,13 +108,9 @@ class InputEditor {
     return removeBtn;
   }
 
-  _setTokens = (tokens: string[] | string) => {
-    const tokenList: string[] = Array.isArray(tokens) ? tokens : [tokens];
-    if (!tokens || !tokenList.length) {
-      return;
-    }
-    tokenList.map((token: string): void => {
-      const tokenElement = this._createTokenElement(token);
+  _setTokens = (tokens: string[]): void | undefined => {
+    tokens.map((token: string): void => {
+      const tokenElement: HTMLElement = this._createTokenElement(token);
       this._container.insertBefore(tokenElement, this._input);
       this._tokens.push(token);
       this._tokenElements.push(tokenElement);
@@ -120,42 +118,50 @@ class InputEditor {
     this._input.value = '';
   };
 
-  _handleTokensAdded = () => {
-    const tokens = this._input.value.replace(/(,\s*$)|(^,*)|(\s)/g, '').split(/[\s,]/g).filter(token => token);
-    tokens.length && this._setTokens(tokens);
-  };
-
-  _removeToken = (index: number) => {
-    const tokenElement = this._container.children[index];
-    if (!tokenElement) {
-      return;
+  _checkIsToken = (tokens: string[] | string): void | undefined => {
+    if (typeof tokens === 'string' && !!tokens) {
+      const validTokens: string[] = tokens.replace(/(,\s*$)|(^,*)|(\s)/g, '').split(/[\s,]/g).filter(token => token);
+      !!validTokens.length && this._setTokens(validTokens);
     }
-    this._container.removeChild(tokenElement);
-    this._tokens.splice(index, 1);
-    this._tokenElements.splice(index, 1);
+    if (Array.isArray(tokens) && !!tokens.length) {
+      const validTokens: string[] = tokens.map((token: string): string => {
+        return token.replace(/(,\s*$)|(^,*)|(\s)/g, '');
+      });
+      !!validTokens.length && this._setTokens(validTokens);
+    }
+    return;
+  }
+
+  _removeToken = (tokenIdx: number): void => {
+    const tokenElement: Node = this._container.children[tokenIdx];
+    if (tokenElement) {
+      this._container.removeChild(tokenElement);
+      this._tokens.splice(tokenIdx, 1);
+      this._tokenElements.splice(tokenIdx, 1);
+    }
   };
 
-  _handleInputFocus = () => {
+  _handleInputFocus = (): void => {
     this._input.focus();
   };
 
-  _handleKeyDown = (event: KeyboardEvent) => {
+  _handleKeyDown = (event: KeyboardEvent): void => {
     const { key } = event;
     if (key === 'Enter') {
-      this._handleTokensAdded();
+      this._checkIsToken(this._input.value);
     }
     if (key === 'Backspace') {
       this._input.selectionEnd === 0 && this._removeToken(this._tokens.length - 1);
     }
   };
 
-  _handleInput = () => {
-    const hasSeparator = /[\s,]/.test(this._input.value);
-    hasSeparator && this._handleTokensAdded();
+  _handleInput = (): void => {
+    const hasComma: boolean = /[\s,]/.test(this._input.value);
+    hasComma && this._checkIsToken(this._input.value);
   };
 
-  _handleBlur = () => {
-    this._handleTokensAdded();
+  _handleBlur = (): void => {
+    this._checkIsToken(this._input.value);
   };
 }
 
